4f3a7211935a10935ef7ed7ef62e07433bc0af5a
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-02 20:05:50 +0300

    odevLoops oluşturuldu odev detayları eklendi

---
+++ odevLoops.js
@@ -0,0 +1,10 @@
+/* 
+1- "Tech Career" metnini tersten yazdır
+
+2- var numbers = [-22, 5, 11, 78, -4, -250, 35, 56, 11, 24, 77, 11] bu dizideki SADECE poizitif sayıların toplamını ekrana yaz
+
+3- var numbers = [-22, 5, 11, 78, -4, -250, 35, 56, 11, 24, 77, 11] bu dizideki en büyük sayı hangisidir?
+
+4- 1-50 arası sayılar içinde 3’e veya 5’e tam bölünebilen sayıları console a yaz ve kaç adet olduğunu console yaz. */
+
+/* ------------------------------------------ */
