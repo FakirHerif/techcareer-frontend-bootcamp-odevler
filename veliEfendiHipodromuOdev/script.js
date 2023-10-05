ee5f0974e91f527d611474004a92584ee045bedc
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-05 04:01:54 +0300

    at yarışı ödevi tamamlandı(stiller ayarlandı ve artık kazanan alert yerine span içinde bildiriliyor, yarış bitine kadar hidden)

--- veliEfendiHipodromuOdev/script.js
+++ veliEfendiHipodromuOdev/script.js
@@ -1,10 +1,18 @@
 function start() {
+
+    document.querySelector(".span").style = "visibility: hidden"
+    document.querySelector(".winner").style = "visibility: hidden"
+
+    const startButton = document.getElementById("startButton");
+
+    startButton.disabled = true;
+    
     const atlar = document.querySelectorAll(".at");
     console.log(atlar);
 
     let finishedCount = 0;
 
-    atlar.forEach((at, index) => {
+    atlar.forEach((at) => {
         let total = 0;
         const yaris = setInterval(() => {
 
@@ -14,16 +22,21 @@ function start() {
 
             at.style.marginLeft = total + "px";
 
-            if (total >= window.innerWidth - 700) {
+            if (total >= window.innerWidth - 100) {
                 clearInterval(yaris);
                 finishedCount++; 
 
                 if (finishedCount === 1) {
-                    alert(`Kazanan: ${at.alt}`);
+                    console.log(`--- Kazanan: ${at.alt} ---`);
+                    document.querySelector(".span").innerHTML = `-----> KAZANAN: ${at.alt.toUpperCase()} <-----`
+
+                    document.querySelector(".span").style = "visible"
+                    document.querySelector(".winner").style = "visible"
                 }
 
                 if (finishedCount === atlar.length) {
                     console.log("Yarış bitti!");
+                    startButton.disabled = false;
                 }
             }
         }, 100);
