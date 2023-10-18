91fd57b354d42c30cc164bbfb6e8babc8493d552
FakirHerif <basdemir35@hotmail.com.tr>
2023-10-18 23:02:33 +0300

    used useEffect and axios for suppliers datas

--- odevSuppliersReact/odev-suppliers-react/src/pages/Suppliers.jsx
+++ odevSuppliersReact/odev-suppliers-react/src/pages/Suppliers.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 function Suppliers() {
+
+    const [suppliers, setSuppliers] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://northwind.vercel.app/api/suppliers')
+        .then(res => {
+            setSuppliers(res.data)
+        })
+    }, [])
+    
+
   return (
-    <div>Suppliers</div>
+    <>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>COMPANY NAME</th>
+                <th>CONTACT NAME</th>
+                <th>CONTACT TITLE</th>
+                <th>ADDRESS</th>
+            </tr>
+        </thead>
+    </table>
+    <tbody>
+        {
+            suppliers.map(suppliers => {
+                return <tr>
+                    <td>{suppliers.id}</td>
+                    <td>{suppliers.companyName}</td>
+                    <td>{suppliers.contactName}</td>
+                    <td>{suppliers.contactTitle}</td>
+                    <td>
+                        <div>
+                            Street: {suppliers.address.street}
+                            <br />
+                            City: {suppliers.address.city}
+                            <br />
+                            Region: {suppliers.address.region}
+                            <br />
+                            Postal Code: {suppliers.address.postalcode}
+                            <br />
+                            Country: {suppliers.address.country}
+                            <br />
+                            Phone: {suppliers.address.phone}
+                        
+                        </div>
+                    </td>
+                    <td><button onClick={() => delete(suppliers.id)}>Delete</button></td>
+                </tr>
+            })
+        }
+    </tbody>
+    </>
   )
 }
 
